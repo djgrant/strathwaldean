84b68dcf130881fb1d485cc7cd55ac58f9e871cd
Daniel Grant <danieljohngrant@gmail.com>
2020-04-22 18:07:09 +0100

    Add survey and facebook group

--- src/routes/get-involved.svelte
+++ src/routes/get-involved.svelte
@@ -8,4 +8,17 @@
   <title>Get involved | Strathwaldean Quad</title>
 </svelte:head>
 
-<p>We'd love to know what you think</p>
+<p>
+  It'd be great to know how everyone thinks the quad should be developed. If
+  you're a resident please
+  <a href="https://forms.gle/hdZTR15MsQ9RUz9h6">take this (short) survey</a>
+  .
+</p>
+
+<p>
+  For those that are on Facebook, we have a
+  <a href="https://www.facebook.com/Strathwaldean-Quad-109529100733700">
+    Facebook group
+  </a>
+  to help stay connected.
+</p>
