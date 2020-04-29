831daf0a162674af586dcfcb4d7d07dd23fe8b4e
Daniel Grant <danieljohngrant@gmail.com>
2020-04-29 18:11:57 +0100

    Improve nav spacing

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -33,21 +33,11 @@
   }
 
   ul {
+    display: flex;
+    justify-content: space-evenly;
     justify-self: end;
     margin: 0;
     padding: 0;
-  }
-
-  /* clearfix */
-  ul::after {
-    content: "";
-    display: block;
-    clear: both;
-  }
-
-  li {
-    display: block;
-    float: left;
   }
 
   [aria-current] {
@@ -58,7 +48,7 @@
   [aria-current]::after {
     position: absolute;
     content: "";
-    width: calc(100% - 1em);
+    width: 100%;
     height: 2px;
     background-color: rgb(61, 207, 61);
     display: block;
@@ -67,11 +57,19 @@
 
   li a {
     text-decoration: none;
-    padding: 1.5em 0.5em;
     display: block;
     font-size: 16px;
     font-weight: normal;
     color: var(--brown);
+  }
+
+  @media (min-width: 601px) {
+    li a {
+      padding: 1.5em 0.5em;
+    }
+    [aria-current]::after {
+      width: calc(100% - 1em);
+    }
   }
 
   @media (max-width: 600px) {
@@ -84,19 +82,30 @@
     }
 
     ul {
+      width: 100%;
       margin: 0 -0.7em;
       align-self: center;
     }
 
     li a {
       font-size: 15px;
-      padding: 1em 0.3em;
+      padding: 1em 0;
     }
   }
 
   @media (max-width: 360px) {
     li a {
       font-size: 14px;
+    }
+  }
+
+  @media (max-width: 300px) {
+    ul {
+      flex-direction: column;
+    }
+
+    li a {
+      padding: 0.3em 0;
     }
   }
 </style>
