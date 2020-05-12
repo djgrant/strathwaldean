bf9e586dbc02f4087e634e989a165025fc355178
Daniel Grant <danieljohngrant@gmail.com>
2020-05-12 17:49:57 +0100

    Filter out draft posts

--- src/routes/posts/index.json.js
+++ src/routes/posts/index.json.js
@@ -8,9 +8,23 @@ export async function get(req, res) {
   });
 
   const posts = await api.posts
-    .browse()
+    .browse({ include: "tags" })
     .then(
-      map(pick(["slug", "title", "created_at", "excerpt", "feature_image"]))
+      map(
+        pick([
+          "slug",
+          "title",
+          "created_at",
+          "excerpt",
+          "feature_image",
+          "tags",
+        ])
+      )
+    )
+    .then((posts) =>
+      process.env.NODE_ENV === "development"
+        ? posts
+        : posts.filter((p) => !p.tags.find((t) => t.name === "draft"))
     );
 
   res.end(JSON.stringify(posts));
